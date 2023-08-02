a5ed54dca4ab88f34939fca9a573ec4fe302bcad
TheBlackEntity <36412486+TheBlackEntity@users.noreply.github.com>
2023-08-02 15:28:45 +0200

    Fix plugin, add customizable sounds, add "openshulker" command

--- src/main/java/me/entity303/openshulker/OpenShulker.java
+++ src/main/java/me/entity303/openshulker/OpenShulker.java
@@ -1,7 +1,13 @@
 package me.entity303.openshulker;
 
+import me.entity303.openshulker.commands.OpenShulkerCommand;
+import me.entity303.openshulker.listener.ShulkerDupeListener;
+import me.entity303.openshulker.listener.ShulkerOpenCloseListener;
+import me.entity303.openshulker.listener.ShulkerReadOnlyListener;
 import me.entity303.openshulker.util.ShulkerActions;
 import org.bukkit.Bukkit;
+import org.bukkit.Sound;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
@@ -12,9 +18,50 @@ public final class OpenShulker extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
+        this.saveDefaultConfig();
+
+        this.reloadConfig();
+
+        String openSound = this.getConfig().getString("OpenSound");
+
+        try {
+            Sound.valueOf(openSound);
+        } catch (Throwable ignored) {
+            if (openSound == null) {
+                Bukkit.getLogger().severe("You did not specify OpenSound, using default");
+                this.getConfig().set("OpenSound", "BLOCK_SHULKER_BOX_OPEN");
+                this.saveConfig();
+                this.reloadConfig();
+            } else
+                Bukkit.getLogger().severe("There is no sound called '" + openSound + "', for a list of sounds, visit https://www.spigotmc.org/wiki/cc-sounds-list/");
+        }
+
+        String closeSound = this.getConfig().getString("CloseSound");
+
+        try {
+            Sound.valueOf(closeSound);
+        } catch (Throwable ignored) {
+            if (closeSound == null) {
+                Bukkit.getLogger().severe("You did not specify CloseSound, using default");
+                this.getConfig().set("OpenSound", "BLOCK_SHULKER_BOX_CLOSE");
+                this.saveConfig();
+                this.reloadConfig();
+            } else
+                Bukkit.getLogger().severe("There is no sound called '" + closeSound + "', for a list of sounds, visit https://www.spigotmc.org/wiki/cc-sounds-list/");
+        }
+
         this.shulkerActions = new ShulkerActions(this);
 
-        Bukkit.getPluginManager().registerEvents(this, this);
+        Bukkit.getPluginManager().registerEvents(new ShulkerOpenCloseListener(this), this);
+        Bukkit.getPluginManager().registerEvents(new ShulkerDupeListener(this), this);
+        Bukkit.getPluginManager().registerEvents(new ShulkerReadOnlyListener(this), this);
+
+        OpenShulkerCommand openShulkerCommand = new OpenShulkerCommand(this);
+
+        PluginCommand command = this.getCommand("openshulker");
+
+        command.setExecutor(openShulkerCommand);
+        command.setTabCompleter(openShulkerCommand);
     }
 
     @Override
